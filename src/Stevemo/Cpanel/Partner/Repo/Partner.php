6c3ef534a62b7bf6e9b58f55444d9fa1977231f7
Henry Ventura <hvent90@gmail.com>
2015-08-06 12:40:10 -0400

    Added partners folder

---
+++ src/Stevemo/Cpanel/Partner/Repo/Partner.php
@@ -0,0 +1,13 @@
+<?php  namespace Stevemo\Cpanel\Partner\Repo;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Partner extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'partners';
+}
